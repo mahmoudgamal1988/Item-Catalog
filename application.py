bea4728a071ba23ab72f0accaa9f2e5cd9296017
Mahmoud Gamal <40371141+mahmoudgamal1988@users.noreply.github.com>
2019-03-20 13:45:23 +0000

    Update application.py

--- application.py
+++ application.py
@@ -21,7 +21,9 @@ APPLICATION_NAME = "Restaurant Menu Application"
 
 
 # Connect to Database and create database session
-engine = create_engine('sqlite:///restaurantmenuapp.db')
+engine = create_engine(
+    'sqlite:///restaurantmenuapp.db', connect_args={'check_same_thread': False}
+    )
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
